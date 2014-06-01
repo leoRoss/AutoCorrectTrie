02f685ba528f4f3240dec8e497524f6076d6155b
Leo Rossignac-Milon <leo.milon@gmail.com>
2014-06-01 09:50:30 -0700

    Update and rename DictionaryTree.py to DictionaryTrie.py

--- DictionaryTrie.py
+++ DictionaryTrie.py
@@ -1,38 +1,40 @@
 #Leo Rossignac-Milon
 #Auto-Correct Suggestor Implemented in a Tree Dictionary - a Trie
 
-#Very rapidly finds all suggested words within a desired distance of a query
-#Distance = # of removals, additions, or character changes need
+#The Dictionary Trie stores (word, frequency) pairs in a Trie:
 
-# (Word, frequency) suggestion pairs are added to the tree
-#
-#                 ''  (Root node = empty string)
+#                  ''  (Root node = empty string)
 #               /    \
-#              h      a ('a', 17)
-#            /   \
-# ("he",13) e     i ("Hi", 7)
-#                /  \
-#     ("him",8) m    l
-#                     \
-#                      l ("hill", 2)
+#               h      a ('a', 17)
+#             /   \
+#  ('he',13) e     i ('Hi', 7)
+#                 /  \
+#      ('him',8) m    l
+#                      \
+#                       l ('hill', 2)
 
-#A query (such as 'hikl') is pushed down the tree
-#As we decend the tree, we edit the start of the query to match the chosen path
-#Each traversal keeps track of the how many edits have been used so far
-#When too many edits were used, that traversal ends
-#If we reach the end of a query and arrive at suggestion, we suggest it!
+# It can very rapidly find all the words in the tree within a specified edit distance of a query string.
+#     Edit Distance = # of edits need to manipulate the query into a word.
+#     1 Edit = removing/adding a single character from query string OR changing a single character in the string to another character.
 
-#For example, if query = 'hikl', we could arrive at the "him"-node in multiple ways:
+
+# A query (such as 'hikl') is pushed down the tree.
+# As we decend the Trie, we edit the query as needed to match the path of the Trie we are decending.
+# Each traversal keeps track of the how many edits have been used so far
+#     If a traversal has used too many edits, that traversal ends
+#     If we arrive at word and we have matched or edited ALL of the characters in the query, we suggest the word in the Trie!
+
+# For example, if query = 'hikl', we could arrive at the "him"-node in multiple ways-
 
 #       add 'm' at query[2]                 ==>     query = 'himkl', edits used = 1
 #       change 'k' for 'm'                  ==>     query = 'himl', edits used = 1
 #       change 'k' for 'm' AND remove 'l'   ==>     query = 'him', edit used = 2 (Return "him" as a suggestion)
 #       remove 'k' AND change 'l' for 'm'   ==>     query = 'him', edit used = 2 (Return "him" as a suggestion)
 
-#As we can see, some combinations of actions are equivalent. 
-#The findAll() method is implemented to reduce - not eliminate - these repetitions.
-#Adding complexity to further reduce repeated work has only resulted in longer overall runtime.
-
+# As we can see, some combinations of actions are equivalent. 
+# The DictionaryTree.findAll() method is implemented to reduce - not eliminate - these repetitions.
+#     Adding complexity to further reduce repeated work has only resulted in longer overall runtime.
+#     Thus, it is necessary for the Trie to remove any duplicate suggestions.
 
 
 #ACTIONS
@@ -44,8 +46,8 @@ START=3
 
 
 
-#Dictionary Tree as described above
-class Trie:
+#Dictionary Trie as described above
+class Trie
     def __init__(self):
         self.root = LetterNode('')
 
